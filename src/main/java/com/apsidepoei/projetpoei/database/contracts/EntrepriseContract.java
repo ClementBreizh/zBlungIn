de5eb4d9080ef19a660f709d9ca7ee8b74f99551
Benjamin Morvan <morvan.benjamin35@gmail.com>
2019-08-20 14:47:07 +0200

    EntrepriseContract

---
+++ src/main/java/com/apsidepoei/projetpoei/database/contracts/EntrepriseContract.java
@@ -0,0 +1,33 @@
+package com.apsidepoei.projetpoei.database.contracts;
+
+public class EntrepriseContract extends BaseContract {
+
+    public final static String TABLE = "Entreprise";
+
+    public final static String COL_ID = "id";
+    public final static String COL_NOM = "nom";
+    public final static String COL_NOM_ANTENNE = "nomAntenne";
+    public final static String COL_SIRET = "siret";
+    public final static String COL_CODE_APE = "codeApe";
+
+    public final static String[] COLS = new String[] {
+            COL_ID,
+            COL_NOM,
+            COL_NOM_ANTENNE,
+            COL_SIRET,
+            COL_CODE_APE,
+    };
+
+    public final static String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS "+ TABLE +"(" +
+            COL_ID + " int NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+            COL_NOM + " VARCHAR(50) NOT NULL," +
+            COL_NOM_ANTENNE + " VARCHAR(50) NOT NULL," +
+            COL_SIRET + " VARCHAR(50) NOT NULL" +
+            COL_CODE_APE + "VARCHAR(5) NOT NULL" +
+            ")";
+
+    public EntrepriseContract() {
+        super(TABLE, COL_ID, COLS, CREATE_TABLE);
+    }
+
+}
