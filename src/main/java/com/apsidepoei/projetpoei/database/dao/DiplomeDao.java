fed9b14ba9a66f7267c2b3148f81d334feaf69ea
Thomas R <thomas.rousseau@protonmail.com>
2019-08-20 14:12:30 +0200

    translation Diplome > Degree

--- src/main/java/com/apsidepoei/projetpoei/database/dao/DiplomeDao.java
+++ src/main/java/com/apsidepoei/projetpoei/database/dao/DiplomeDao.java
@@ -5,36 +5,35 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.ParseException;
 
-import com.apsidepoei.projetpoei.database.contracts.DiplomeContract;
-import com.apsidepoei.projetpoei.mysql.entities.Diplome;
-import com.poei_juillet_2019.mysql.entities.Entreprise;
+import com.apsidepoei.projetpoei.database.contracts.DegreeContract;
+import com.apsidepoei.projetpoei.mysql.entities.Degree;
 
-public class DiplomeDao extends BaseDao<Diplome> {
+public class DegreeDao extends BaseDao<Degree> {
 
-	public DiplomeDao() {
-        super(new DiplomeContract());
+	public DegreeDao() {
+        super(new DegreeContract());
     }
 
     @Override
-    protected void javaToSqlInsert(Diplome item, PreparedStatement ps) throws SQLException {
+    protected void javaToSqlInsert(Degree item, PreparedStatement ps) throws SQLException {
         ps.setString(2, item.getName());
         ps.setString(3, item.getLevel());
     }
 
     @Override
-    protected void javaToSqlUpdate(Diplome item, PreparedStatement ps) throws SQLException {
+    protected void javaToSqlUpdate(Degree item, PreparedStatement ps) throws SQLException {
         ps.setString(1, item.getName());
         ps.setString(2, item.getLevel());
         ps.setString(3, item.getId());
     }
 
     @Override
-    protected Diplome parseFromDbToJava(ResultSet rs) throws SQLException, ParseException {
-        Diplome item = new Diplome();
+    protected Degree parseFromDbToJava(ResultSet rs) throws SQLException, ParseException {
+    	Degree item = new Degree();
 
-        item.setId(rs.getInt(rs.findColumn(DiplomeContract.COL_ID)));
-        item.setName(rs.getString(rs.findColumn(DiplomeContract.COL_NAME)));
-        item.setLevel(rs.getString(rs.findColumn(DiplomeContract.COL_LEVEL)));
+        item.setId(rs.getInt(rs.findColumn(DegreeContract.COL_ID)));
+        item.setName(rs.getString(rs.findColumn(DegreeContract.COL_NAME)));
+        item.setLevel(rs.getString(rs.findColumn(DegreeContract.COL_LEVEL)));
 
         return item;
     }
