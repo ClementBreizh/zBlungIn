878516367f2077dcd989b6b24e36564ad0d201ed
Thomas R <thomas.rousseau@protonmail.com>
2019-08-29 10:22:27 +0200

    updating Appointment controller

--- src/main/java/com/apsidepoei/projetpoei/controllers/AppointmentController.java
+++ src/main/java/com/apsidepoei/projetpoei/controllers/AppointmentController.java
@@ -1,15 +1,7 @@
 package com.apsidepoei.projetpoei.controllers;
 
-import java.text.ParseException;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import com.apsidepoei.projetpoei.controllers.restcontrollers.AppointmentRestController;
-import com.apsidepoei.projetpoei.controllers.restcontrollers.base.BaseRestController;
-import com.apsidepoei.projetpoei.database.repositories.AppointmentRepository;
-import com.apsidepoei.projetpoei.entities.Appointment;
 
 /**
  * Controller for appointment form.
