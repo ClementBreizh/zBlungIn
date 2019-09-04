a92fab4e34e99c309947be3530936246f3183fa8
Clement BOUCHEREAU <clementbreizhweb@gmail.com>
2019-09-04 13:16:22 +0200

    RestController fix missing extends

--- src/main/java/com/apsidepoei/projetpoei/controllers/restcontrollers/CompanyCandidatesSessionRestController.java
+++ src/main/java/com/apsidepoei/projetpoei/controllers/restcontrollers/CompanyCandidatesSessionRestController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apsidepoei.projetpoei.controllers.restcontrollers.base.BaseRestController;
 import com.apsidepoei.projetpoei.database.repositories.CompanyCandidatesSessionRepository;
+import com.apsidepoei.projetpoei.entities.CompanyCandidatesSession;
 
 /**
  * @author vianney
@@ -15,8 +17,8 @@ import com.apsidepoei.projetpoei.database.repositories.CompanyCandidatesSessionR
  */
 @RestController
 @RequestMapping("/api/candidBySessionAndCompany")
-public class CompanyCandidatesSessionRestController {
+public class CompanyCandidatesSessionRestController extends BaseRestController<CompanyCandidatesSession, Integer> {
   public CompanyCandidatesSessionRestController(@Autowired CompanyCandidatesSessionRepository repository) {
-    super();
+    super(repository);
   }
 }
