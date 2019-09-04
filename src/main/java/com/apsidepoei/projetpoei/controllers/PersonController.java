7aab76880aa6135c47ddb4fdc45a3aa61a2fae05
Tshark-VR <vianney.rslt@gmail.com>
2019-09-04 22:06:02 +0200

    getAllUserByFirstnameLastname en cours

--- src/main/java/com/apsidepoei/projetpoei/controllers/PersonController.java
+++ src/main/java/com/apsidepoei/projetpoei/controllers/PersonController.java
@@ -1,7 +1,16 @@
 package com.apsidepoei.projetpoei.controllers;
 
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.apsidepoei.projetpoei.database.repositories.PersonRepository;
+import com.apsidepoei.projetpoei.entities.Person;
 
 /**
  * Controller for persons form.
@@ -11,6 +20,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/persons")
 public class PersonController extends BaseController {
+
+  @Autowired
+  private PersonRepository repository;
+
+  @GetMapping("/userByFLName")
+  public List<Person> getAllUserByFirstnameLastname(@PathParam(value = "firstname") String firstname,
+      @PathParam(value = "lastname") String lastname) {
+    return (List<Person>) repository.findAllByFirstnameAndLastname(firstname, lastname);
+  }
 
   /**
    * Constructor.
--- src/main/java/com/apsidepoei/projetpoei/database/repositories/PersonRepository.java
+++ src/main/java/com/apsidepoei/projetpoei/database/repositories/PersonRepository.java
@@ -1,8 +1,12 @@
 package com.apsidepoei.projetpoei.database.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.apsidepoei.projetpoei.entities.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+  List<Person> findAllByFirstnameAndLastname(String firstname, String lastname);
 
 }
