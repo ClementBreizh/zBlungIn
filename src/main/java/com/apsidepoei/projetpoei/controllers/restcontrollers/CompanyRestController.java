ef4bf894dfd5777c58b9f040aefec361df1c4aba
Thomas R <thomas.rousseau@protonmail.com>
2019-09-24 23:59:12 +0200

    updated Company controller

--- src/main/java/com/apsidepoei/projetpoei/controllers/restcontrollers/CompanyRestController.java
+++ src/main/java/com/apsidepoei/projetpoei/controllers/restcontrollers/CompanyRestController.java
@@ -1,7 +1,11 @@
 package com.apsidepoei.projetpoei.controllers.restcontrollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apsidepoei.projetpoei.controllers.restcontrollers.base.BaseRestController;
@@ -17,5 +21,20 @@ import com.apsidepoei.projetpoei.entities.Company;
 public class CompanyRestController extends BaseRestController<Company, Integer> {
   public CompanyRestController(@Autowired CompanyRepository repository) {
     super(repository);
+  }
+
+  @GetMapping("filtered")
+  public Page<Company> getAllFiltered(
+    final Pageable pageable,
+    @RequestParam(defaultValue = "") final String name,
+    @RequestParam(defaultValue = "") final String antennaName,
+    @RequestParam(defaultValue = "") final String siret,
+    @RequestParam(defaultValue = "") final String apeCode,
+    @RequestParam(defaultValue = "") final String homePhone){
+    return this.getRepository().findAll(pageable);
+  }
+
+  protected CompanyRepository getRepository() {
+    return (CompanyRepository) this.repository;
   }
 }
